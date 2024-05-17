913e55e9229acaa3d8c80728e358d8ecd8639e94
s21dlemanskiy <55732402+s21dlemanskiy@users.noreply.github.com>
2024-05-17 17:13:07 +0300

    final ( i guess )

--- build_presintation.py
+++ build_presintation.py
@@ -3,6 +3,7 @@ from typing import List, Tuple, Dict, Any, Union
 from collections.abc import Collection
 import json
 from pptx_renderer import PPTXRenderer
+from numpy import polyfit, poly1d
 
 
 #https://issekdash.hse.ru/viewer/public?dashboardGuid=0e54f8988a3241129ffae56ca76001cd&showNav=true
@@ -23,7 +24,17 @@ def select(selected_year: int, sector: str, data: Dict[str, Any]):
 
 
 def my_plot(plt, bar_names: List[str], raw_values: List[Tuple[float, float]], title: str = "title", figsize=(20, 10),
-            width=0.8, magic_values=(0.1, 0.1, 0), anotate_diff=True, anotate_diff_round=1):
+            width=0.8, magic_values=(0.34, 0.42, 0), anotate_diff_percent=True, anotate_diff_values=True, anotate_diff_round=1,
+            general_font_size=10, axes_font_size=10, axis_title_font_size=10, title_fontsize=10, y_lab=None, y_bounds=None):
+    """define some global size vars"""
+    plt.rc('font', size=general_font_size)  # controls default text sizes
+    plt.rc('axes', titlesize=axis_title_font_size)  # fontsize of the axes title
+    plt.rc('axes', labelsize=axes_font_size)  # fontsize of the x and y labels
+    # plt.rc('xtick', labelsize=size)  # fontsize of the tick labels
+    # plt.rc('ytick', labelsize=size)  # fontsize of the tick labels
+    # plt.rc('legend', fontsize=size)  # legend fontsize
+    plt.rc('figure', titlesize=title_fontsize)  # fontsize of the figure title
+    """calculate and define some important values"""
     colors = []
     values = []
     arrow_data = []
@@ -35,34 +46,47 @@ def my_plot(plt, bar_names: List[str], raw_values: List[Tuple[float, float]], ti
         else:
             values += [after_val, before_val]
             colors += ["green", "grey"]
-        arrow_data.append({"after_val": after_val, "before_val": before_val, "x": x + magic_values[0], "y": after_val,
+        arrow_data.append({"after_val": after_val, "before_val": before_val,
+                           "x": x + width * magic_values[0], "y": after_val,
                            "dy": after_val - before_val,
-                           "defference": f'{round(abs(before_val - after_val) / before_val * 100, 1)}%',
-                           "defference_pos": (x - magic_values[1], magic_values[2] + (before_val + after_val) / 2)})
+                           "defference": f'{"-" if before_val > after_val else "+"}{round(abs(before_val - after_val) / before_val * 100, 1)}%',
+                           "defference_pos": (x - width * magic_values[1], magic_values[2] * max(after_val, before_val) + (before_val + after_val) / 2),
+                           "color": colors[-2]})
         x += 1
+    """style"""
+    plt.ylabel(y_lab)
     plt.figure(figsize=figsize)
-    plt.title(title)
+    plt.title(title, fontsize=title_fontsize)
     double_bar_names = sum(map(list, zip(bar_names, bar_names)), [])
-    # print(double_bar_names, values, colors)
+    if y_bounds is not None:
+        plt.ylim(*y_bounds)
+    """ploting"""
     plt.bar(double_bar_names, values, color=colors, width=width)
+    """add arrow and some text"""
     for arrow in arrow_data:
+        """arrow"""
         plt.annotate('',
                      xy=(arrow["x"], arrow["y"]),
                      xytext=(arrow["x"], arrow["y"] - arrow["dy"]),
                      arrowprops={'arrowstyle': "->"})
-        # plt.arrow(x= h + 0.3, y= data["пассажирооборот"][i][2] , dx= 0 , dy= data["пассажир
-        plt.annotate(arrow["defference"], xy=arrow["defference_pos"])
-        if anotate_diff:
+        """percent"""
+        if anotate_diff_percent:
+            percent_anotaton = plt.annotate(arrow["defference"], xy=arrow["defference_pos"])
+            percent_anotaton.set_bbox(dict(facecolor='white', alpha=0.7, edgecolor=arrow["color"], boxstyle='round', pad=0.3))
+        """plt val was to val become"""
+        if anotate_diff_values:
             plt.annotate(
                 f"{round(arrow['before_val'], anotate_diff_round)}->{round(arrow['after_val'], anotate_diff_round)}",
                 xy=(arrow["defference_pos"][0], max(arrow["after_val"], arrow["before_val"]) + 0.1))
+    if y_bounds is not None:
+        plt.ylim(*y_bounds)
 
 
 # indexes = data.keys()
 # sectors = set(map(lambda x: x["idParts"][-1], sum(map(lambda x: x["colsInfo"], data.values()), [])))
 # my_plot(plt, ['all', 'дальнего следования', 'пригородного сообщения'], [(133.3, 78.1), (99.1, 120.5), (34.4, 24.7)])
 def create_my_plot(sector, index, year1, year2, y_lab=None, **kwargs):
-    global data
+    # varible [data] from other scope
     try:
         valeues1, names1 = zip(*select(year1, sector=sector, data=data[index]))
         valeues2, names2 = zip(*select(year2, sector=sector, data=data[index]))
@@ -75,10 +99,12 @@ def create_my_plot(sector, index, year1, year2, y_lab=None, **kwargs):
     plt.clf()
     if "title" not in kwargs:
         kwargs["title"] = f"Динамика изменения {year1}-{year2} показателя «{index}»"
-    my_plot(plt, names,
-            list(zip(valeues1, valeues2)), **kwargs)
     if y_lab is None:
-        plt.ylabel(index.split(",")[-1])
+        y_lab = index.split(",")[-1]
+    """ploting"""
+    my_plot(plt, names,
+            list(zip(valeues1, valeues2)), y_lab=y_lab, **kwargs)
+    """save plot to png local file"""
     link = f'pictures/{sector.replace(" ", "_").replace(".", "").replace(",", "")}_{index.replace(" ", "_")}_{year1}_{year2}.png'
     plt.savefig(link)
     plt.close()
@@ -110,13 +136,32 @@ def process_data(data: Dict[str,Any]):
                 break
 
 
+def add_trend_to_plot(plt, x, y, *args, **kwargs):
+    to_float = lambda arr: [i for i in range(0, len(arr))]
+    z = polyfit(to_float(x), y, 1)
+    p = poly1d(z)
+    plt.plot(x, p(to_float(x)), **kwargs)
+
 def create_my_plot2(sector: str, year: int = None, group: int = 1, month: str = None, title: str = None,
-                    figsize=(20, 10)):
+                    figsize=(20, 10), general_font_size=10, axes_font_size=10, axis_title_font_size=10, title_fontsize=10,
+                    y_bounds=(0, 500), trend=True, y_lab=None):
+    # varible [data2] from other scope
+    """define some global size vars"""
+    plt.rc('font', size=general_font_size)  # controls default text sizes
+    plt.rc('axes', titlesize=axis_title_font_size)  # fontsize of the axes title
+    plt.rc('axes', labelsize=axes_font_size)  # fontsize of the x and y labels
+    # plt.rc('xtick', labelsize=size)  # fontsize of the tick labels
+    # plt.rc('ytick', labelsize=size)  # fontsize of the tick labels
+    # plt.rc('legend', fontsize=size)  # legend fontsize
+    plt.rc('figure', titlesize=title_fontsize)  # fontsize of the figure title
+    """some work with data"""
     p_data = list(process_data(data2[sector]))
     if year is not None:
         p_data = list(filter(lambda x: int(x["year"]) == year, p_data))
     if month is not None:
         p_data = list(filter(lambda x: x["month"] == month, p_data))
+
+    """if we want to group by n month - do it"""
     if group != 1:
         x = []
         y = []
@@ -133,15 +178,57 @@ def create_my_plot2(sector: str, year: int = None, group: int = 1, month: str = 
     else:
         x = list(map(lambda x: x["month"] + "\n" + x["year"], p_data))
         y = list(map(lambda x: x["value"], p_data))
+    """plot figure"""
     plt.figure(figsize=figsize)
-    plt.ylim(0, 300)
+    if trend:
+        add_trend_to_plot(plt, x, y, color="grey", linestyle="--")
     plt.plot(x, y)
-    plt.ylabel("млрд руб")
+    """ add some styling """
+    plt.ylim(*y_bounds)
+    if y_lab is None:
+        y_lab = "млрд руб"
+    plt.ylabel(y_lab)
     if title is None:
-        title = f"Динамика изменения оборота cетора «{sector}»"
-    plt.title(title)
+        title = f"Динамика изменения оборота cектора «{sector}»"
+    plt.title(title, fontsize=title_fontsize)
+    """save plot to png local file"""
     filename = sector.replace(" ", "_").replace(".", "").replace(",", "") + str(group) + (
         str(year) if year is not None else "") + (str(month) if month is not None else "")
+    link = f'pictures/{filename}.png'
+    plt.savefig(link)
+    plt.close()
+    return link
+
+
+def create_my_plot3(sector_list: str, title: str = None,
+                    figsize=(20, 10), general_font_size=10, axes_font_size=10, axis_title_font_size=10, title_fontsize=10,
+                    y_bounds=(0, 500), y_lab=None):
+    # varible [data2] from other scope
+    """define some global size vars"""
+    plt.rc('font', size=general_font_size)  # controls default text sizes
+    plt.rc('axes', titlesize=axis_title_font_size)  # fontsize of the axes title
+    plt.rc('axes', labelsize=axes_font_size)  # fontsize of the x and y labels
+    plt.rc('figure', titlesize=title_fontsize)  # fontsize of the figure title
+    plt.figure(figsize=figsize)
+    for sector in sector_list:
+        """some work with data"""
+        p_data = list(process_data(data2[sector]))
+        x = list(map(lambda x: x["month"] + "\n" + x["year"], p_data))
+        y = list(map(lambda x: x["value"], p_data))
+        """plot figure"""
+        plt.plot(x, y, label=sector)
+    plt.legend(loc="upper left")
+    """ add some styling """
+    plt.ylim(*y_bounds)
+    if y_lab is None:
+        y_lab = "млрд руб"
+    plt.ylabel(y_lab)
+    if title is None:
+        title = f"Динамика изменения оборота cекторов ИКТ"
+    plt.title(title, fontsize=title_fontsize)
+    """save plot to png local file"""
+    sector_format = lambda sector: sector.replace(" ", "_").replace(".", "").replace(",", "")
+    filename = "my_plot3" + "_".join(list(map(sector_format, sector_list)))
     link = f'pictures/{filename}.png'
     plt.savefig(link)
     plt.close()
@@ -152,15 +239,24 @@ def avg_round_by_sector(sector, round_num=2):
     return round(avg(list(map(lambda x: x["value"], list(process_data(data2[sector]))))), round_num)
 
 
+SMALL_FONT = 8
+MEDIUM_FONT = 10
+BIGGER_FONT = 12
 
 p = PPTXRenderer("Tamplate.pptx")
 p.render(
     "output.pptx",
     {
         "create_my_plot": create_my_plot,
+        "create_my_plot3":create_my_plot3,
         "data": data,
         "avg":avg,
         "avg_round_by_sector":avg_round_by_sector,
-        "create_my_plot2":create_my_plot2
+        "create_my_plot2":create_my_plot2,
+        "SMALL_FONT": SMALL_FONT,
+        "MEDIUM_FONT":MEDIUM_FONT,
+        "BIGGER_FONT":BIGGER_FONT,
     }
 )
+
+
